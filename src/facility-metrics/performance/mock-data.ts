8329f2210d3379a926e9c5348bb2f8e5675ee225
Isaac Akileng <isaacakileng56@gmail.com>
2023-10-26 18:01:00 +0300

    Add performance place holders

---
+++ src/facility-metrics/performance/mock-data.ts
@@ -0,0 +1,269 @@
+enum LegendPositions {
+  RIGHT = "right",
+  LEFT = "left",
+  TOP = "top",
+  BOTTOM = "bottom",
+}
+
+enum ScaleTypes {
+  LABELS = "labels",
+  LABELS_RATIO = "labels_ratio",
+  LINEAR = "linear",
+  LOG = "log",
+  TIME = "time",
+}
+
+export const donutGenderData = [
+  {
+    group: "Male",
+    value: 11251,
+  },
+  {
+    group: "Female",
+    value: 17955,
+  },
+];
+
+export const donutGenderOptions = {
+  title: "Patients by Gender",
+  resizable: true,
+  legend: {
+    position: LegendPositions.TOP,
+  },
+  donut: {
+    center: {
+      label: "Total",
+    },
+  },
+  height: "300px",
+};
+
+export const donutDepartmentData = [
+  {
+    group: "TB Clinic",
+    value: 20000,
+  },
+  {
+    group: "MCH Clinic",
+    value: 65000,
+  },
+  {
+    group: "Out Patient",
+    value: 75000,
+  },
+  {
+    group: "Inpatient",
+    value: 1200,
+  },
+  {
+    group: "ART Clinic",
+    value: 10000,
+  },
+  {
+    group: "LAB",
+    value: 25000,
+  },
+];
+
+export const donutDepartmentOptions = {
+  title: "Patients by Department",
+  resizable: true,
+  donut: {
+    center: {
+      label: "Patients",
+    },
+  },
+  height: "300px",
+};
+
+export const stackedBarData = [
+  {
+    group: "InPatient",
+    key: "Jan",
+    value: 32432,
+  },
+  {
+    group: "InPatient",
+    key: "Feb",
+    value: 21312,
+  },
+  {
+    group: "InPatient",
+    key: "March",
+    value: 56456,
+  },
+  {
+    group: "InPatient",
+    key: "April",
+    value: 21312,
+  },
+  {
+    group: "InPatient",
+    key: "May",
+    value: 34234,
+  },
+  {
+    group: "InPatient",
+    key: "Jun",
+    value: 32432,
+  },
+  {
+    group: "InPatient",
+    key: "Jul",
+    value: 21312,
+  },
+  {
+    group: "InPatient",
+    key: "Aug",
+    value: 56456,
+  },
+  {
+    group: "InPatient",
+    key: "Sept",
+    value: 21312,
+  },
+  {
+    group: "InPatient",
+    key: "Oct",
+    value: 34234,
+  },
+  {
+    group: "InPatient",
+    key: "Nov",
+    value: 21312,
+  },
+  {
+    group: "InPatient",
+    key: "Dec",
+    value: 56456,
+  },
+  {
+    group: "OutPatient",
+    key: "Jan",
+    value: 12312,
+  },
+  {
+    group: "OutPatient",
+    key: "Feb",
+    value: 23232,
+  },
+  {
+    group: "OutPatient",
+    key: "March",
+    value: 34232,
+  },
+  {
+    group: "OutPatient",
+    key: "April",
+    value: 12312,
+  },
+  {
+    group: "OutPatient",
+    key: "May",
+    value: 34234,
+  },
+  {
+    group: "OutPatient",
+    key: "Jun",
+    value: 23232,
+  },
+  {
+    group: "OutPatient",
+    key: "Jul",
+    value: 34232,
+  },
+  {
+    group: "OutPatient",
+    key: "Aug",
+    value: 12312,
+  },
+  {
+    group: "OutPatient",
+    key: "Sept",
+    value: 34234,
+  },
+  {
+    group: "OutPatient",
+    key: "Oct",
+    value: 34232,
+  },
+  {
+    group: "OutPatient",
+    key: "Nov",
+    value: 12312,
+  },
+  {
+    group: "OutPatient",
+    key: "Dec",
+    value: 34234,
+  },
+];
+
+export const stackedBarOptions = {
+  title: "Patients",
+  legend: {
+    position: LegendPositions.TOP,
+  },
+  axes: {
+    left: {
+      mapsTo: "value",
+      stacked: true,
+    },
+    bottom: {
+      mapsTo: "key",
+      scaleType: ScaleTypes.LABELS,
+    },
+  },
+  height: "500px",
+  width: "40rem",
+};
+
+export const horizontalBarData = [
+  {
+    group: "ART",
+    value: 65000,
+  },
+  {
+    group: "TB",
+    value: 29123,
+  },
+  {
+    group: "OPD",
+    value: 35213,
+  },
+  {
+    group: "MCH",
+    value: 51213,
+  },
+  {
+    group: "IPD",
+    value: 16932,
+  },
+];
+
+export const horizontalBarOptions = {
+  title: "Departments",
+  legend: {
+    position: LegendPositions.TOP,
+  },
+  axes: {
+    left: {
+      mapsTo: "group",
+      scaleType: ScaleTypes.LABELS,
+    },
+    bottom: {
+      mapsTo: "value",
+    },
+  },
+  height: "450px",
+  grid: {
+    x: {
+      enabled: true,
+      numberOfTicks: 3,
+    },
+    y: {
+      enabled: true,
+      numberOfTicks: 6,
+    },
+  },
+};
