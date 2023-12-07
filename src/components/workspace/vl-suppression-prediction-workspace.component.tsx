ca23903dfb413e514b0dbd2e4edb09f85f2ea90c
Daphne <89844768+Daphne210@users.noreply.github.com>
2023-12-07 08:49:49 +0300

    Changing url parameter names (#16)
    * Changing url param names
    
    * Getting ART Start Date, patient uuid, concept uuid

--- src/components/workspace/vl-suppression-prediction-workspace.component.tsx
+++ src/components/workspace/vl-suppression-prediction-workspace.component.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { InlineLoading } from "@carbon/react";
 import styles from "./vl-suppression-prediction.scss";
-import { useVLSuppressionDetails } from "./vl-suppression-prediction.resource";
+import {
+  useGetARTStartDate,
+  useVLSuppressionDetails,
+} from "./vl-suppression-prediction.resource";
 import { ErrorState } from "@openmrs/esm-framework";
 
 const VLSuppressionPredictionWorkSpace: React.FC = () => {
@@ -9,23 +12,43 @@ const VLSuppressionPredictionWorkSpace: React.FC = () => {
   const [artStartDate, setArtStartDate] = useState("2020-04-20");
   const [dateOfBirth, setDateOfBirth] = useState("1992-04-20");
   const [gender, setGender] = useState("Female");
-  const [arvAdherence, setArvAdherence] = useState("Good");
+  const [arvAdherence, setArvAdherence] = useState("90157");
   const [currentRegimen, setCurrentRegimen] = useState("TDF-3TC-DTG");
   const [indicationForVLTesting, setIndicationForVLTesting] =
     useState("168684");
 
+  const [patientUuid, setPatientUuid] = useState(
+    "93e4e7e1-c916-47d3-b00d-c7c0aa6d1ce6"
+  );
+  const [conceptUuid, setConceptUuid] = useState(
+    "ab505422-26d9-41f1-a079-c3d222000440"
+  );
+  const { artStartDateData, isLoading, conceptuuid, patientuuid } =
+    useGetARTStartDate({
+      patientuuid: patientUuid,
+      conceptuuid: conceptUuid,
+    });
+
   const { data, isErrorInSendingRequest, isLoadingPrediction } =
     useVLSuppressionDetails({
-      encounter_date: encounterDate,
+      last_encounter_date: encounterDate,
       art_start_date: artStartDate,
       date_birth: dateOfBirth,
       gender: gender,
-      arv_adherence: arvAdherence,
+      last_arv_adherence: arvAdherence,
       current_regimen: currentRegimen,
-      Indication_for_VL_Testing: indicationForVLTesting,
+      last_indication_for_VL_Testing: indicationForVLTesting,
     });
 
+  if (isLoading && patientuuid && conceptuuid) {
+    setPatientUuid(patientuuid);
+    setConceptUuid(conceptuuid);
+  }
+
   if (isLoadingPrediction) {
+    console.info("ART START DATE", artStartDateData);
+    console.info("Concept uuid", conceptuuid);
+    console.info("Person uuid", patientuuid);
     return (
       <InlineLoading
         status="active"
--- src/components/workspace/vl-suppression-prediction.resource.ts
+++ src/components/workspace/vl-suppression-prediction.resource.ts
@@ -1,14 +1,15 @@
 import useSWR from "swr";
 import axios from "axios";
+import { openmrsFetch } from "@openmrs/esm-framework";
 
 interface SaveParams {
-  encounter_date: string;
+  last_encounter_date: string;
   art_start_date: string;
   date_birth: string;
   gender: string;
-  arv_adherence: string;
+  last_arv_adherence: string;
   current_regimen: string;
-  Indication_for_VL_Testing: string;
+  last_indication_for_VL_Testing: string;
 }
 
 interface PredictionData {
@@ -16,6 +17,11 @@ interface PredictionData {
     Client: string;
   };
 }
+
+type ARTStartDateRequest = {
+  conceptuuid: string;
+  patientuuid: string;
+};
 
 export function useVLSuppressionDetails(params: SaveParams) {
   const apiUrl = "https://ai.mets.or.ug/predict";
@@ -44,5 +50,33 @@ export function useVLSuppressionDetails(params: SaveParams) {
     isErrorInSendingRequest: error,
     isLoadingPrediction: isLoading,
     isValidatingParams: isValidating,
+  };
+}
+
+function extractDate(timestamp: string): string {
+  const dateObject = new Date(timestamp);
+  const year = dateObject.getFullYear();
+  const month = (dateObject.getMonth() + 1).toString().padStart(2, "0");
+  const day = dateObject.getDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
+
+export function useGetARTStartDate(params: ARTStartDateRequest) {
+  const apiUrl = `/ws/rest/v1/obs?concept=${params.conceptuuid}&patient=${params.patientuuid}&v=full`;
+  const { data, error, isLoading, isValidating, mutate } = useSWR<
+    { data: { results: any } },
+    Error
+  >(apiUrl, openmrsFetch);
+  const artStartDateData = data ? extractDate(data.data.results[0].value) : [];
+  const conceptuuid = data ? data?.data.results[0].concept.uuid : null;
+  const patientuuid = data ? data?.data.results[0].person.uuid : null;
+  return {
+    artStartDateData,
+    conceptuuid,
+    patientuuid,
+    isError: error,
+    isLoading: isLoading,
+    mutate,
   };
 }
